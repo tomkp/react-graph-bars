fc1a7ea04e9adcdb866298dd595c598efa22168a
tomkp <tom@tomkp.com>
2016-07-12 19:58:09 +0100

    examples

--- demo/Application.js
+++ demo/Application.js
@@ -18,7 +18,15 @@ const Application = () => {
     };
     return (
         <div className="application">
-            <Graph name="xxx" data={generateData(50)} />
+            <Graph name="xxx" data={generateData(50)} minColor="black" maxColor="yellow" />
+            <Graph name="xxx" data={generateData(50)} minColor="red" maxColor="yellow" />
+            <Graph name="xxx" data={generateData(50)} minColor="red" maxColor="green" />
+            <Graph name="xxx" data={generateData(50)} minColor="blue" maxColor="green" />
+            <Graph name="xxx" data={generateData(50)} minColor="blue" maxColor="red" />
+            <Graph name="xxx" data={generateData(50)} minColor="rgba(0,0,0,.5)" maxColor="red" />
+            <Graph name="xxx" data={generateData(50)} minColor="rgba(0,0,0,.8)" maxColor="green" />
+            <Graph name="xxx" data={generateData(50)} minColor="rgba(0,0,255,.5)" maxColor="rgba(255,0,0,.5)" />
+            <Graph name="xxx" data={generateData(50)} minColor="rgba(255,0,100,1)" maxColor="rgba(0,0,255,.5)" />
         </div>
     );
 };
--- src/Graph.js
+++ src/Graph.js
@@ -16,4 +16,6 @@ const Bar = ({value, minColor = 'rgba(0, 0, 255, .75)', maxColor = 'rgba(255, 0,
 };
 
 
-export default ({name, data}) => <div className={`graph ${name}`}>{ data.map((value, index) => <Bar key={index} value={value} />)}</div>
+export default ({name, data, minColor = 'rgba(0, 0, 255, .75)', maxColor = 'rgba(255, 0, 0, .75)'}) => {
+ return <div className={`graph ${name}`}>{ data.map((value, index) => <Bar key={index} value={value} minColor={minColor} maxColor={maxColor} />)}</div>
+}
